9c08fd21b241ca19c385466d30ae4be6057f32a5
hata0 <haataa0100@gmail.com>
2024-10-07 16:55:44 +0900

    feat: 雰囲気を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function TopPage() {
   return (
     <div>
-      <h1>Top Page</h1>
+      <div>コンピュータ研究部</div>
+      <div>Illustration</div>
+      <div>イラストスライダー</div>
+      <div>作品カード</div>
+      <div>作品カード</div>
+      <div>作品カード</div>
+      <div>作品カード</div>
     </div>
   );
 }
