42f2cfc741bba4f1b1dd4e8e8fa41902e70015cd
hata0 <haataa0100@gmail.com>
2024-10-09 17:07:31 +0900

    feat: Creations コンポーネントに分割

---
+++ src/features/top/components/Creations/index.tsx
@@ -0,0 +1,3 @@
+export const Creations = () => {
+  return <div>Creations</div>;
+};
--- src/features/top/components/Top/index.tsx
+++ src/features/top/components/Top/index.tsx
@@ -1,14 +1,12 @@
+import { Creations } from "../Creations";
 import { Pictures } from "../Pictures";
-
 export const Top = () => {
   return (
     <div>
       <div>コンピュータ研究部</div>
       <div>Pictures</div>
       <Pictures />
-      <div>作品カード</div>
-      <div>作品カード</div>
-      <div>作品カード</div>
+      <Creations />
     </div>
   );
 };
