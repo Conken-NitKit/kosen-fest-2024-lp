310c622fac5e81113fc0e21fbcc85cc82dc773c5
hata0 <haataa0100@gmail.com>
2025-02-26 12:43:30 +0900

    feat: アニメーション追加

--- src/features/top/components/pictures-carousel/index.tsx
+++ src/features/top/components/pictures-carousel/index.tsx
@@ -3,8 +3,10 @@ import {
   UncontainedCarouselItem,
 } from "@/components/my-ui/carousel/uncontained";
 import { cn } from "@/lib/utils";
+import { useInView } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 export type Picture = {
   src: string;
@@ -16,11 +18,19 @@ type Props = {
   pictures: Picture[];
 };
 export const PicturesCarousel = ({ pictures }: Props) => {
+  const ref = useRef<HTMLElement>(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <UncontainedCarousel
       className="h-[400px] w-full xl:w-[1280px]"
       loop
       autoScrollOptions={{ playOnInit: true }}
+      // initアニメーション
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={isInView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 1 }}
     >
       {pictures.map(({ src, alt, id }) => (
         <UncontainedCarouselItem key={id}>
