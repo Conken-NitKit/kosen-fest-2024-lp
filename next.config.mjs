7d4e79d46211645c120d810b561d001579580b52
hata0 <haataa0100@gmail.com>
2025-02-27 15:36:20 +0900

    fix: デプロイが通らない問題を修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
 export default nextConfig;
