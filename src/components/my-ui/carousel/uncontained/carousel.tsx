596e12e7140af505c007a91cfaba65c24ae19b65
hata0 <haataa0100@gmail.com>
2025-02-27 11:36:16 +0900

    feat: UncontainedCarouselをmotionに依存しないように修正

--- src/components/my-ui/carousel/uncontained/carousel.tsx
+++ src/components/my-ui/carousel/uncontained/carousel.tsx
@@ -3,25 +3,28 @@
 import { cn } from "@/lib/utils";
 import AutoScroll, { type AutoScrollOptionsType } from "embla-carousel-auto-scroll";
 import useEmblaCarousel from "embla-carousel-react";
-import { type HTMLMotionProps, motion } from "motion/react";
-import type { PropsWithChildren } from "react";
+import type { ComponentProps, PropsWithChildren, ReactNode } from "react";
+import { Slot } from "../../core/slot";
 
 type Props = {
   className: string;
   loop?: boolean;
   autoScrollOptions?: AutoScrollOptionsType;
-} & Omit<HTMLMotionProps<"section">, "children">;
+  element?: ReactNode;
+} & Omit<ComponentProps<"section">, "children">;
 /**
  * 画像の幅に合わせてカルーセルを表示するコンポーネント
  * @param props.className - これでheight, widthを画面サイズに応じて指定する必要がある
  * @param props.loop - ループするかどうか
  * @param props.autoScrollOptions - オートスクロールの設定
+ * @param props.element - 別の要素にしたいときに設定
  * @param props.children - UncontainedCarouselItemを渡す
  */
 export const UncontainedCarousel = ({
   className,
   loop,
   autoScrollOptions,
+  element,
   children,
   ...props
 }: PropsWithChildren<Props>) => {
@@ -35,13 +38,13 @@ export const UncontainedCarousel = ({
   );
 
   return (
-    <motion.section {...props}>
+    <Slot element={element ? element : <section />} {...props}>
       {/* カルーセルの表示領域。ここにheight, widthを適用 */}
       <div ref={emblaRef} className={cn("overflow-hidden", className)}>
         {/* コンテナ。ここにrefでtransformが当たることでスクロールして移動する */}
         {/* gapで指定するとうまくいかないためこちらに-mlを指定 */}
         <ul className="-ml-padding-8 flex h-full">{children}</ul>
       </div>
-    </motion.section>
+    </Slot>
   );
 };
